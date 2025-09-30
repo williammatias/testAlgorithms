eaf89dca74a1c5f1bf74a9d1b016a1e19680fd88
William Matias <williammatias@Williams-MacBook-Air.local>
2025-09-29 20:58:09 -0400

    adding recursive binary search

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -1,3 +1,21 @@
 def recursive_binary_search(list, target):
     if len(list) == 0:
         return False
+    else:
+        midpoint= (len(list))//2
+        if list[midpoint] == target:
+            return True
+        if list[midpoint] < target:
+            return recursive_binary_search(list[midpoint+1:], target)
+        else:
+            return recursive_binary_search(list[:midpoint], target)
+
+def verify_binary_search(result):
+    print("Verifying Binary Search", result)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = recursive_binary_search(numbers, 12)
+verify_binary_search(result)
+
+result = recursive_binary_search(numbers, 6)
+verify_binary_search(result)
